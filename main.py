052ce1cb26884e1e93a6c63ad8210395f5e8fcd0
James Xue <jamesxue100@gmail.com>
2015-10-09 19:17:57 -0400

    import flask

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
+import flask
 from flask import Flask, render_template, json, request, redirect
 
 import base64
