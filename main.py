9b1eb4b46a50153a68eab428ae23e28df2c6b954
James Xue <jamesxue100@gmail.com>
2015-10-09 17:06:54 -0400

    removed else statement for run_flow

--- main.py
+++ main.py
@@ -97,10 +97,10 @@ def get_credentials():
     if not credentials or credentials.invalid:
         flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
         flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatability with Python 2.6
-            credentials = tools.run(flow, store)
+        # if flags:
+        credentials = tools.run_flow(flow, store, flags)
+        # else: # Needed only for compatability with Python 2.6
+            # credentials = tools.run(flow, store)
         print('Storing credentials to ' + credential_path)
     return credentials
 
